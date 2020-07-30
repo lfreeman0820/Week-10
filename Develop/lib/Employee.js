47ef46d2b37fd4cd048eda1346fd5d724bc9109f
lfreeman0820 <lfreeman0820@gmail.com>
2020-07-30 16:33:39 -0700

    done!

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,24 @@
 // TODO: Write code to define and export the Employee class
+
+class Employee {
+    constructor(name, id, email, role = "Employee"){
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.role = role;
+    }
+    getName(){
+        return this.name
+    }
+    getEmail(){
+        return this.email
+    }
+    getId(){
+        return this.id
+    }
+    getRole(){
+        return this.role
+    }
+}
+
+module.exports = Employee
--- Develop/test/Intern.test.js
+++ Develop/test/Intern.test.js
@@ -2,7 +2,7 @@ const Intern = require("../lib/Intern");
 
 test("Can set school via constructor", () => {
   const testValue = "UCLA";
-  const e = new Intern("Foo", 1, "test@test.com", testValue);
+  const e = new Intern("Foo", 1, "test@test.com",  testValue);
   expect(e.school).toBe(testValue);
 });
 
